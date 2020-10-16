4c372ae718df2241b6a6e27f2f3fca79d98a4a55
Gaurav Chaurasia <gaurav.181cv155@nitk.edu.in>
2020-10-17 01:43:28 +0530

    add error handeler helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+    def application_error_messages_helper(resource)
+        return '' if resource.errors.empty?
+
+        messages = resource.errors.full_messages.map { |message| content_tag(:li, message) }.join
+        sentence = I18n.t(
+            'errors.messages.not_saved',
+            count: resource.errors.count,
+            resource: resource.class.model_name.human.downcase
+        )
+
+        html = <<-HTML
+        <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+            <h4 class="alert-heading">#{sentence}</h4>
+            <ul class="mb-0">#{messages}</ul>
+        </div>
+        HTML
+
+        html.html_safe
+    end
 end
