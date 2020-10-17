d8344525703d6b79845ac8008c8dcd79d4bb81aa
Gaurav Chaurasia <gaurav.181cv155@nitk.edu.in>
2020-10-17 19:56:24 +0530

    add controller and routes for request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,3 +1,28 @@
 class RequestsController < ApplicationController 
+    before_action :authenticate_user!
+    
+    # Requests: GET
+    #show all request assosiated to a specific book/:book_id/requests
+    def index 
+        @requests = Request.where(books_id: params[:book_id])
+    end    
+    
 
+    def create
+        request = Book.createRequest(params[:book_id])
+        respond_to do |format|
+            if request
+                format.html { redirect_to root_path, notice: 'Request was successfully created.' }
+            else
+                format.html { redirect_to root_path, notice: 'Something went wrong request not created!' } 
+            end
+        end
+    end
+
+
+    private
+        
+    def request_params
+        params.require(:request).permit(:book_id, :id)
+    end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,19 @@
 class Request < ApplicationRecord
     belongs_to :user
     belongs_to :book
+
+    enum status: [:pending, :approved, :rejected]
+
+    def self.createRequest(book_id)
+        book = Book.find(book_id)
+        if book && book.quantity > 0
+            book.quantity -= 1
+            request = Request.new(user: current_user, book: book)
+            if book.save and request.save
+                request
+            end
+        end
+        return nil
+    end
+
 end
